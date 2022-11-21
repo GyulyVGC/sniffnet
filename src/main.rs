9637cf885923256e368d0fa42f7501cad76f8433
Giuliano Bellini s294739 <s294739@studenti.polito.it>
2022-11-21 23:02:21 +0100

    added constraint on minum window size allowed

--- src/main.rs
+++ src/main.rs
@@ -75,7 +75,7 @@ pub fn main() -> iced::Result {
         window: window::Settings {
             size: (1190, 670), // start size
             position: Position::Centered,
-            min_size: Some((1190, 0)), // min size allowed
+            min_size: Some((1190, 500)), // min size allowed
             max_size: None,
             resizable: true,
             decorations: true,
