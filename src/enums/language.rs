3605ae46530f26a4d3ce6b5b28a8aa468445c291
Giuliano Bellini s294739 <s294739@studenti.polito.it>
2023-03-03 18:41:13 +0100

    added support for ukrainian characters

--- src/enums/language.rs
+++ src/enums/language.rs
@@ -29,7 +29,7 @@ impl Default for Language {
 
 impl Language {
     pub(crate) const COL1: [Language; 4] = [Language::EN, Language::FR, Language::ES, Language::ZH];
-    pub(crate) const COL2: [Language; 3] = [Language::DE, Language::IT, Language::PL];
+    pub(crate) const COL2: [Language; 4] = [Language::DE, Language::IT, Language::PL, Language::UA];
     pub fn get_radio_label(&self) -> &str {
         match self {
             Language::EN => "English",
@@ -38,7 +38,7 @@ impl Language {
             Language::ES => "Español",
             Language::PL => "Polski",
             Language::DE => "Deutsch",
-            Language::UA => "Українська"
+            Language::UA => "Українська",
             Language::ZH => "简体中文",
         }
     }
