d2f958bda66436e5b260107b10bf269459801cbd
Dominic Kim <44078909+eatingdm@users.noreply.github.com>
2023-05-21 12:13:07 +0900

    Add Korean translation(translations_2.rs)
    Translate everything into native Korean

--- src/translations/translations_2.rs
+++ src/translations/translations_2.rs
@@ -9,6 +9,7 @@ pub fn new_version_available_translation(language: Language) -> &'static str {
         Language::RU => "Новая версия доступна на GitHub",
         Language::EL => "Μια νεότερη έκδοση είναι διαθέσιμη στο GitHub",
         Language::FA => "یک نسخه جدیدتر روی GitHub موجود است",
+        Language::KO => "GitHub에 새로운 버전이 출시되었습니다.",
         _ => "A newer version is available on GitHub",
     }
 }
@@ -22,6 +23,7 @@ pub fn inspect_translation(language: Language) -> &'static str {
         Language::PL => "Sprawdź",
         Language::DE => "Überprüfen",
         Language::RU => "Инспектировать",
+        Language::KO => "검사",
         _ => "Inspect",
     }
 }
@@ -30,6 +32,7 @@ pub fn connection_details_translation(language: Language) -> &'static str {
     match language {
         Language::EN => "Connection details",
         Language::IT => "Dettagli della connessione",
+        Language::KO => "연결 상세",
         _ => "Connection details",
     }
 }
@@ -38,6 +41,7 @@ pub fn dropped_packets_translation(language: Language) -> &'static str {
     match language {
         Language::EN => "Dropped packets",
         Language::IT => "Pacchetti mancati",
+        Language::KO => "손실 패킷",
         _ => "Dropped packets",
     }
 }
@@ -46,6 +50,7 @@ pub fn data_representation_translation(language: Language) -> &'static str {
     match language {
         Language::EN => "Data representation",
         Language::IT => "Rappresentazione dei dati",
+        Language::KO => "데이터 단위",
         _ => "Data representation",
     }
 }
@@ -54,6 +59,7 @@ pub fn host_translation(language: Language) -> &'static str {
     match language {
         Language::EN => "Network host",
         Language::IT => "Host di rete",
+        Language::KO => "네트워크 호스트",
         _ => "Network host",
     }
 }
@@ -62,6 +68,7 @@ pub fn only_top_30_hosts_translation(language: Language) -> &'static str {
     match language {
         Language::EN => "Only the top 30 hosts are displayed here",
         Language::IT => "Solo i maggiori 30 host sono mostrati qui",
+        Language::KO => "상위 30개의 호스트만 노출됩니다",
         _ => "Only the top 30 hosts are displayed here",
     }
 }
@@ -70,6 +77,7 @@ pub fn sort_by_translation(language: Language) -> &'static str {
     match language {
         Language::EN => "Sort by",
         Language::IT => "Ordina per",
+        Language::KO => "정렬",
         _ => "Sort by",
     }
 }
@@ -78,6 +86,7 @@ pub fn local_translation(language: Language) -> String {
     match language {
         Language::EN => "Local network",
         Language::IT => "Rete locale",
+        Language::KO => "로컬 네트워크",
         _ => "Local network",
     }
     .to_string()
@@ -87,6 +96,7 @@ pub fn unknown_translation(language: Language) -> String {
     match language {
         Language::EN => "Unknown location",
         Language::IT => "Localizzazione sconosciuta",
+        Language::KO => "알 수 없는 위치",
         _ => "Unknown location",
     }
     .to_string()
@@ -96,6 +106,7 @@ pub fn your_network_adapter_translation(language: Language) -> String {
     match language {
         Language::EN => "Your network adapter",
         Language::IT => "La tua scheda di rete",
+        Language::KO => "네트워크 어댑터",
         _ => "Your network adapter",
     }
     .to_string()
@@ -105,6 +116,7 @@ pub fn socket_address_translation(language: Language) -> &'static str {
     match language {
         Language::EN => "Socket address",
         Language::IT => "Indirizzo del socket",
+        Language::KO => "소켓 어드레스",
         _ => "Socket address",
     }
 }
@@ -113,6 +125,7 @@ pub fn mac_address_translation(language: Language) -> &'static str {
     match language {
         Language::EN => "MAC address",
         Language::IT => "Indirizzo MAC",
+        Language::KO => "맥 어드레스",
         _ => "MAC address",
     }
 }
@@ -121,6 +134,7 @@ pub fn source_translation(language: Language) -> &'static str {
     match language {
         Language::EN => "Source",
         Language::IT => "Sorgente",
+        Language::KO => "소스",
         _ => "Source",
     }
 }
@@ -129,6 +143,7 @@ pub fn destination_translation(language: Language) -> &'static str {
     match language {
         Language::EN => "Destination",
         Language::IT => "Destinazione",
+        Language::KO => "목적지",
         _ => "Destination",
     }
 }
@@ -137,6 +152,7 @@ pub fn fqdn_translation(language: Language) -> &'static str {
     match language {
         Language::EN => "Fully qualified domain name",
         Language::IT => "Nome di dominio completo",
+        Language::KO => "절대 도메인 네임",
         _ => "Fully qualified domain name",
     }
 }
@@ -145,6 +161,7 @@ pub fn administrative_entity_translation(language: Language) -> &'static str {
     match language {
         Language::EN => "Administrative entity",
         Language::IT => "Entità amministrativa",
+        Language::KO => "관리 엔티티",
         _ => "Administrative entity",
     }
 }
@@ -153,6 +170,7 @@ pub fn transmitted_data_translation(language: Language) -> &'static str {
     match language {
         Language::EN => "Transmitted data",
         Language::IT => "Dati trasmessi",
+        Language::KO => "수신된 데이터",
         _ => "Transmitted data",
     }
 }
@@ -161,6 +179,7 @@ pub fn country_translation(language: Language) -> &'static str {
     match language {
         Language::EN => "Country",
         Language::IT => "Paese",
+        Language::KO => "국가",
         _ => "Country",
     }
 }
@@ -169,6 +188,7 @@ pub fn domain_name_translation(language: Language) -> &'static str {
     match language {
         Language::EN => "Domain name",
         Language::IT => "Nome di dominio",
+        Language::KO => "도메인 네임",
         _ => "Domain name",
     }
 }
@@ -177,6 +197,7 @@ pub fn only_show_favorites_translation(language: Language) -> &'static str {
     match language {
         Language::EN => "Only show favorites",
         Language::IT => "Mostra solo i preferiti",
+        Language::KO => "즐겨찾기만 보기",
         _ => "Only show favorites",
     }
 }
@@ -185,6 +206,7 @@ pub fn search_filters_translation(language: Language) -> &'static str {
     match language {
         Language::EN => "Search filters",
         Language::IT => "Filtri di ricerca",
+        Language::KO => "검색 필터",
         _ => "Search filters",
     }
 }
@@ -193,6 +215,7 @@ pub fn no_search_results_translation(language: Language) -> &'static str {
     match language {
         Language::EN => "No result available according to the specified search filters",
         Language::IT => "Nessun risultato disponibile secondo i filtri di ricerca specificati",
+        Language::KO => "해당 검색 필터로 검색된 결과가 없습니다.",
         _ => "No result available according to the specified search filters",
     }
 }
@@ -206,6 +229,7 @@ pub fn showing_results_translation(
     match language {
         Language::EN => format!("Showing {start}-{end} of {total} total results"),
         Language::IT => format!("Sono mostrati {start}-{end} di {total} risultati totali"),
+        Language::KO => format!("총 {total}개의 결과 중 {start}-{end}을(를) 보여줍니다")
         _ => format!("Showing {start}-{end} of {total} total results"),
     }
 }
@@ -215,6 +239,7 @@ pub fn color_gradients_translation(language: Language) -> &'static str {
     match language {
         Language::EN => "Apply color gradients",
         Language::IT => "Applica sfumature di colore",
+        Language::KO => "그라디언트 색상 적용",
         _ => "Apply color gradients",
     }
 }
