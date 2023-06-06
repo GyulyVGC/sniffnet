c8e123f52163f226a90263d488ee659e0ed3cc12
The Artifex (4r7if3x) <the.artifex@proton.me>
2023-06-06 14:54:03 +0200

    Fixed a mistake

--- resources/packaging/macos/wrapper.sh
+++ resources/packaging/macos/wrapper.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env zsh
-osacompile -e "do shell script \"/Applications/Sniffnet.app/Contents/MacOS/sniffnet >/dev/null 2>&1 &\" with administrator privileges"
+osascript -e "do shell script \"/Applications/Sniffnet.app/Contents/MacOS/sniffnet >/dev/null 2>&1 &\" with administrator privileges"
