bacd8d183d2ecc9154e35f978d9e7b3531be0b8f
The Artifex (4r7if3x) <the.artifex@proton.me>
2023-06-07 11:16:44 +0200

    Embedded PS1 scripts into the workflow

--- resources/packaging/windows/scripts/wix.ps1
+++
@@ -1,5 +0,0 @@
-Write-Host "::group::WiX Toolset"
-Invoke-WebRequest -Uri "https://github.com/wixtoolset/wix3/releases/download/wix3112rtm/wix311-binaries.zip" -OutFile "$env:TEMP\\wix-binaries.zip" -Verbose
-Expand-Archive -LiteralPath "$env:TEMP\\wix-binaries.zip" -DestinationPath "$env:TEMP\\wix" -Verbose
-Set-Item -Path env:Path -Value "$env:Path;$env:TEMP\\wix"
-Write-Host "::endgroup::"
