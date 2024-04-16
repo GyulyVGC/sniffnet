25672e6edc82726fbf452f560831d017d3f4b60b
mkadir <muhammadalihakimov03@gmail.com>
2024-04-16 17:46:54 +0500

    translations update with v 1.3 for Uzbek lang

--- src/translations/translations_3.rs
+++ src/translations/translations_3.rs
@@ -13,6 +13,7 @@ pub fn general_translation(language: Language) -> &'static str {
         Language::IT => "Generali",
         Language::FR => "Général",
         Language::DE => "Allgemein",
+        Language::UZ => "Asosiy",
         _ => "General",
     }
 }
@@ -20,6 +21,7 @@ pub fn general_translation(language: Language) -> &'static str {
 pub fn zoom_translation(language: Language) -> &'static str {
     match language {
         Language::EN | Language::IT | Language::ES | Language::FR | Language::DE => "Zoom",
+        Language::UZ => "Kattalashtirish",
         _ => "Zoom",
     }
 }
@@ -31,6 +33,7 @@ pub fn mmdb_files_translation(language: Language) -> &'static str {
         Language::IT => "File di database",
         Language::FR => "Fichiers de la base de données",
         Language::DE => "Datenbank Dateien",
+        Language::UZ => "Ma'lumotlar bazasi fayllari",
         _ => "Database files",
     }
 }
@@ -42,6 +45,7 @@ pub fn params_not_editable_translation(language: Language) -> &'static str {
         Language::IT => "I seguenti parametri non sono modificabili durante l'analisi",
         Language::FR => "Les paramètres suivants ne peuvent pas être modifiés durant l'analyse",
         Language::DE => "Die folgenden Paramter können während der Analyse nicht verändert werden",
+        Language::UZ => "Tahlil vaqtida quydagi parametrlarni o'zgartirib bo'lmaydi",
         _ => "The following parameters can't be modified during the analysis",
     }
 }
@@ -52,6 +56,7 @@ pub fn params_not_editable_translation(language: Language) -> &'static str {
 //         Language::IT => "Percorso del file",
 //         Language::FR => "Chemin du fichier",
 //         Language::DE => "Dateipfad",
+//         Language::UZ => "Fayl yo'li",
 //         _ => "File path",
 //     }
 // }
@@ -63,6 +68,7 @@ pub fn custom_style_translation(language: Language) -> &'static str {
         Language::IT => "Stile personalizzato",
         Language::FR => "Style personnalisé",
         Language::DE => "Benutzerdefinierter Stil",
+        Language::UZ => "Moslashtirilgan uslub",
         _ => "Custom style",
     }
 }
@@ -73,6 +79,7 @@ pub fn copy_translation(language: Language) -> &'static str {
         Language::IT | Language::ES => "Copia",
         Language::FR => "Copie",
         Language::DE => "Kopieren",
+        Language::UZ => "Nusxalash",
         _ => "Copy",
     }
 }
@@ -82,6 +89,7 @@ pub fn port_translation(language: Language) -> &'static str {
         Language::EN | Language::FR | Language::DE => "Port",
         Language::ES => "Puerto",
         Language::IT => "Porta",
+        Language::UZ => "Port",
         _ => "Port",
     }
 }
@@ -93,6 +101,7 @@ pub fn invalid_filters_translation(language: Language) -> &'static str {
         Language::IT => "Filtri non validi",
         Language::FR => "Filtres invalides",
         Language::DE => "Ungültige Filter",
+        Language::UZ => "Noto'g'ri filterlar",
         _ => "Invalid filters",
     }
 }
@@ -103,6 +112,7 @@ pub fn messages_translation(language: Language) -> &'static str {
         Language::ES => "Mensajes",
         Language::IT => "Messaggi",
         Language::DE => "Nachrichten",
+        Language::UZ => "Xabarlar",
         _ => "Messages",
     }
 }
@@ -114,6 +124,7 @@ pub fn link_type_translation(language: Language) -> &'static str {
         Language::IT => "Tipo di collegamento",
         Language::FR => "Type de connexion",
         Language::DE => "Verbindungsart",
+        Language::UZ => "Havola turi",
         _ => "Link type",
     }
 }
@@ -128,6 +139,7 @@ pub fn unsupported_link_type_translation(
         Language::IT => "Il tipo di collegamento associato a questo adattatore di rete non è ancora supportato da Sniffnet...",
         Language::FR => "Le type de connexion associé à cet adaptateur n'est pas encore supporté par Sniffnet...",
         Language::DE => "Die Verbindungsart dieses Adapters wird noch nicht von Sniffnet unterstützt",
+        Language::UZ => "Ushbu adapter bilan bog'langan havola turi hozircha Sniffnet tomonidan qo'llab quvvatlanmaydi",
         _ => "The link type associated with this adapter is not supported by Sniffnet yet...",
     }.to_string();
 
@@ -154,6 +166,7 @@ pub fn database_from_file_translation(language: Language) -> &'static str {
         Language::IT => "Seleziona file di database",
         Language::FR => "Sélection d'un fichier de base de données",
         Language::DE => "Datenbank Datei auswählen",
+        Language::UZ => "Ma'lumotlar bazasi faylini tanlang",
         _ => "Select database file",
     }
 }
@@ -165,6 +178,7 @@ pub fn filter_by_host_translation(language: Language) -> &'static str {
         Language::IT => "Filtra per host di rete",
         Language::FR => "Filtrer par réseau hôte",
         Language::DE => "Nach Netzwerk-Host filtern",
+        Language::UZ => "Tarmoq host bo'yicha filterlash",
         _ => "Filter by network host",
     }
 }
@@ -174,6 +188,7 @@ pub fn service_translation(language: Language) -> &'static str {
         Language::EN | Language::FR | Language::DE => "Service",
         Language::ES => "Servicio",
         Language::IT => "Servizio",
+        Language::UZ => "Xizmat",
         _ => "Service",
     }
 }
@@ -184,6 +199,7 @@ pub fn export_capture_translation(language: Language) -> &'static str {
         Language::IT => "Esporta file di cattura",
         Language::FR => "Exporter le fichier de capture",
         Language::DE => "Aufzeichnungsdatei exportieren",
+        Language::UZ => "Cap faylni export qilish",
         _ => "Export capture file",
     }
 }
@@ -195,6 +211,7 @@ pub fn directory_translation(language: Language) -> &'static str {
         Language::IT => "Cartella",
         Language::FR => "Répertoire",
         Language::DE => "Ordner",
+        Language::UZ => "Katalog",
         _ => "Directory",
     }
 }
@@ -205,6 +222,7 @@ pub fn select_directory_translation(language: Language) -> &'static str {
         Language::IT => "Seleziona cartella di destinazione",
         Language::FR => "Sélectionner le répertoire de destination",
         Language::DE => "Zielorder wählen",
+        Language::UZ => "Manzil katalogni tanlang",
         _ => "Select destination directory",
     }
 }
@@ -215,6 +233,7 @@ pub fn file_name_translation(language: Language) -> &'static str {
         Language::IT => "Nome del file",
         Language::FR => "Nom du fichier",
         Language::DE => "Dateiname",
+        Language::UZ => "Fayl nomi",
         _ => "File name",
     }
 }
@@ -225,6 +244,7 @@ pub fn thumbnail_mode_translation(language: Language) -> &'static str {
         Language::IT => "Modalità miniatura",
         Language::FR => "Mode miniature",
         Language::DE => "Bild-in-Bild Modus",
+        Language::UZ => "Eskiz rejim",
         _ => "Thumbnail mode",
     }
 }
@@ -235,6 +255,7 @@ pub fn learn_more_translation(language: Language) -> &'static str {
         Language::IT => "Vuoi saperne di più?",
         Language::FR => "Voulez-vous en savoir davantage?",
         Language::DE => "Mehr erfahren",
+        Language::UZ => "Ko'proq bilishni hohlaysizmi ?",
         _ => "Do you want to learn more?",
     }
 }
