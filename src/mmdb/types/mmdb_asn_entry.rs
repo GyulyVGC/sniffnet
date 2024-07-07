3a8540a06c267e608a86bcd96f1927a67e38421d
Giuliano Bellini s294739 <s294739@studenti.polito.it>
2024-07-07 16:46:53 +0200

    add generic MmdbCountryEntry to also support IPinfo database entries

---
+++ src/mmdb/types/mmdb_asn_entry.rs
@@ -0,0 +1,11 @@
+// use maxminddb::geoip2;
+//
+// pub enum MmdbAsnEntry<'a> {
+//     Standard(geoip2::Asn<'a>),
+//     Ipinfo(IpinfoAsnEntry<'a>),
+// }
+//
+// pub struct IpinfoAsnEntry<'a> {
+//     pub asn: Option<&'a str>,
+//     pub asn_name: Option<&'a str>,
+// }
--- src/mmdb/types/mod.rs
+++ src/mmdb/types/mod.rs
@@ -1,1 +1,3 @@
+pub mod mmdb_asn_entry;
+pub mod mmdb_country_entry;
 pub mod mmdb_reader;
