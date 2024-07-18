dc369762542a1a9ff8cd55608c2ef279360029de
Giuliano Bellini s294739 <s294739@studenti.polito.it>
2024-07-18 22:11:00 +0200

    test get_asn() and get_country()

--- src/mmdb/types/mmdb_reader.rs
+++ src/mmdb/types/mmdb_reader.rs
@@ -9,16 +9,11 @@ pub enum MmdbReader {
 
 impl MmdbReader {
     pub fn from(mmdb_path: &String, default_mmdb: &'static [u8]) -> MmdbReader {
-        let default_reader = maxminddb::Reader::from_source(default_mmdb).unwrap();
-        if mmdb_path.is_empty() {
-            MmdbReader::Default(default_reader)
-        } else {
-            let custom_reader_result = maxminddb::Reader::open_readfile(mmdb_path);
-            if let Ok(custom_reader) = custom_reader_result {
-                return MmdbReader::Custom(custom_reader);
-            }
-            MmdbReader::Default(default_reader)
+        if let Ok(custom_reader) = maxminddb::Reader::open_readfile(mmdb_path) {
+            return MmdbReader::Custom(custom_reader);
         }
+        let default_reader = maxminddb::Reader::from_source(default_mmdb).unwrap();
+        MmdbReader::Default(default_reader)
     }
 
     pub fn lookup<'a, T: Deserialize<'a>>(&'a self, ip: IpAddr) -> Result<T, MaxMindDBError> {
