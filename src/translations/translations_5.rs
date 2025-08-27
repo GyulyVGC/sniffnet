a1f24584d3f3ca1449ac11d1e38f32fc373c47ee
MelcuGoa <80478973+MelcuGoa@users.noreply.github.com>
2025-08-27 08:58:50 +0300

    Update translations_5.rs
    Added romanian translation

--- src/translations/translations_5.rs
+++ src/translations/translations_5.rs
@@ -6,6 +6,7 @@ pub fn filter_traffic_translation(language: Language) -> String {
     match language {
         Language::EN => "Filter traffic",
         Language::IT => "Filtra il traffico",
+        Language::RO => "Filtrează traficul",
         _ => "Filter traffic",
     }
     .to_string()
@@ -16,6 +17,7 @@ pub fn traffic_source_translation(language: Language) -> &'static str {
     match language {
         Language::EN => "Traffic source",
         Language::IT => "Fonte del traffico",
+        Language::RO => "Sursa traficului",
         _ => "Traffic source",
     }
 }
