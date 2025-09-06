b4d03a84b24191a3ffe7e46004702971053e8ef1
Raghav Potluri <raghav.potluri21@gmail.com>
2025-09-06 01:22:28 -0700

    use -P for pwd

--- resources/packaging/macos/wrapper.sh
+++ resources/packaging/macos/wrapper.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env zsh
-SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd -P)"
 SNIFFNET_PATH="$SCRIPT_DIR/sniffnet"
 osascript -e "do shell script \"'$SNIFFNET_PATH' >/dev/null 2>&1 &\" with prompt \"Comfortably monitor your Internet traffic.\" with administrator privileges"
