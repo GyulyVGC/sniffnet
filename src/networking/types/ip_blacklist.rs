a1fe94aee610424344fb763997449a9223eb62dc
GyulyVGC <gyulyvgc99@gmail.com>
2026-02-01 12:46:17 +0100

    add unit tests on IpBlacklist

--- src/networking/types/ip_blacklist.rs
+++ src/networking/types/ip_blacklist.rs
@@ -41,3 +41,44 @@ impl IpBlacklist {
         self.is_loading = true;
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::net::Ipv4Addr;
+
+    #[tokio::test]
+    async fn test_ip_blacklist_valid() {
+        let blacklist =
+            IpBlacklist::from_file("resources/test/ip_blacklist_valid.txt".to_string()).await;
+
+        assert!(!blacklist.is_invalid());
+        assert!(!blacklist.is_loading());
+        assert_eq!(blacklist.ips.len(), 4);
+
+        assert!(blacklist.contains(&IpAddr::V4(Ipv4Addr::new(8, 8, 8, 8))));
+        assert!(blacklist.contains(&IpAddr::V4(Ipv4Addr::new(1, 2, 3, 255))));
+        assert!(blacklist.contains(&"::123".parse::<IpAddr>().unwrap()));
+        assert!(blacklist.contains(&"fe80::99".parse::<IpAddr>().unwrap()));
+
+        assert!(!blacklist.contains(&IpAddr::V4(Ipv4Addr::new(8, 8, 8, 9))));
+        assert!(!blacklist.contains(&IpAddr::V4(Ipv4Addr::new(10, 2, 3, 255))));
+        assert!(!blacklist.contains(&"::124".parse::<IpAddr>().unwrap()));
+        assert!(!blacklist.contains(&"fe80::98".parse::<IpAddr>().unwrap()));
+    }
+
+    #[tokio::test]
+    async fn test_ip_blacklist_invalid() {
+        let blacklist =
+            IpBlacklist::from_file("resources/test/ip_blacklist_invalid.txt".to_string()).await;
+
+        assert!(blacklist.is_invalid());
+        assert!(!blacklist.is_loading());
+        assert_eq!(blacklist.ips.len(), 0);
+
+        assert!(!blacklist.contains(&IpAddr::V4(Ipv4Addr::new(8, 8, 8, 8))));
+        assert!(!blacklist.contains(&IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0))));
+        assert!(!blacklist.contains(&"::123".parse::<IpAddr>().unwrap()));
+        assert!(!blacklist.contains(&"::".parse::<IpAddr>().unwrap()));
+    }
+}
