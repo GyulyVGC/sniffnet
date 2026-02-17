03722f67941d611bc43e5b90e0492cce65aa3bb6
DeeEffAre <enescaydere@gmail.com>
2026-02-17 10:24:15 +0300

    Add Turkish translation for v1.4 and v1.5

--- src/translations/translations_5.rs
+++ src/translations/translations_5.rs
@@ -7,6 +7,7 @@ pub fn filter_traffic_translation(language: Language) -> String {
         Language::EN => "Filter traffic",
         Language::CS => "Filtr provozu",
         Language::IT => "Filtra il traffico",
+        Language::TR => "Trafiği filtrele",
         _ => "Filter traffic",
     }
     .to_string()
@@ -18,6 +19,7 @@ pub fn traffic_source_translation(language: Language) -> &'static str {
         Language::EN => "Traffic source",
         Language::CS => "Zdroj provozu",
         Language::IT => "Fonte del traffico",
+        Language::TR => "Trafik kaynağı",
         _ => "Traffic source",
     }
 }
@@ -26,6 +28,7 @@ pub fn remote_notifications_translation(language: Language) -> &'static str {
     match language {
         Language::EN => "Remote notifications",
         Language::IT => "Notifiche remote",
+        Language::TR => "Uzak bildirimler",
         _ => "Remote notifications",
     }
 }
@@ -34,6 +37,7 @@ pub fn ip_blacklist_translation(language: Language) -> &'static str {
     match language {
         Language::EN => "IP blacklist",
         Language::IT => "Blacklist IP",
+        Language::TR => "IP kara listesi",
         _ => "IP blacklist",
     }
 }
@@ -42,6 +46,7 @@ pub fn blacklisted_transmitted_translation(language: Language) -> &'static str {
     match language {
         Language::EN => "New data exchanged from a blacklisted IP",
         Language::IT => "Nuovi dati scambiati da un IP in blacklist",
+        Language::TR => "Kara listedeki bir IP ile veri alışverişi yapıldı",
         _ => "New data exchanged from a blacklisted IP",
     }
 }
@@ -50,6 +55,7 @@ pub fn only_show_blacklisted_translation(language: Language) -> &'static str {
     match language {
         Language::EN => "Only show blacklisted",
         Language::IT => "Mostra solo in blacklist",
+        Language::TR => "Sadece kara listedekileri göster",
         _ => "Only show blacklisted",
     }
 }
