1c45be6d618a2701516f010c08a4dbbabad6d5db
Nubi <35731885+nubikripto@users.noreply.github.com>
2026-04-21 08:53:12 +0700

    Add Indonesian 🇮🇩 translations for v1.5

--- src/translations/translations_5.rs
+++ src/translations/translations_5.rs
@@ -12,6 +12,7 @@ pub fn filter_traffic_translation(language: Language) -> String {
         Language::TR => "Trafiği filtrele",
         Language::JA => "トラフィックをフィルタリング",
         Language::RO => "Filtrează traficul",
+        Language::ID => "Filter lalulintas data",
         _ => "Filter traffic",
     }
     .to_string()
@@ -28,6 +29,7 @@ pub fn traffic_source_translation(language: Language) -> &'static str {
         Language::TR => "Trafik kaynağı",
         Language::JA => "トラフィック元",
         Language::RO => "Sursa traficului",
+        Language::ID => "Asal lalulintas data",
         _ => "Traffic source",
     }
 }
@@ -41,6 +43,7 @@ pub fn remote_notifications_translation(language: Language) -> &'static str {
         Language::TR => "Uzak bildirimler",
         Language::JA => "リモート通知",
         Language::RO => "Notificări la distanță",
+        Language::ID => "Pemberitahuan jarak jauh",
         _ => "Remote notifications",
     }
 }
@@ -54,6 +57,7 @@ pub fn ip_blacklist_translation(language: Language) -> &'static str {
         Language::TR => "IP kara listesi",
         Language::JA => "IP ブラックリスト",
         Language::RO => "Blacklist IP-uri",
+        Language::ID => "Daftar hitam IP",
         _ => "IP blacklist",
     }
 }
@@ -67,6 +71,7 @@ pub fn blacklisted_transmitted_translation(language: Language) -> &'static str {
         Language::TR => "Kara listedeki bir IP ile veri alışverişi yapıldı",
         Language::JA => "ブラックリストに登録されたIPから新しいデータが交換されました",
         Language::RO => "Noi date schimbate de la un IP în blacklist",
+        Language::ID => "Data baru didapat dari daftar hitam IP",
         _ => "New data exchanged from a blacklisted IP",
     }
 }
@@ -80,13 +85,14 @@ pub fn only_show_blacklisted_translation(language: Language) -> &'static str {
         Language::TR => "Sadece kara listedekileri göster",
         Language::JA => "ブラックリストのみ表示",
         Language::RO => "Afișează doar blacklist",
+        Language::ID => "Hanya tampilkan daftar hitam",
         _ => "Only show blacklisted",
     }
 }
 
 pub fn program_translation(language: Language) -> &'static str {
     match language {
-        Language::EN | Language::TR | Language::RO => "Program",
+        Language::EN | Language::TR | Language::RO | Language::ID => "Program",
         Language::IT => "Programma",
         Language::ZH => "程序",
         Language::ZH_TW => "程式",
@@ -104,6 +110,7 @@ pub fn no_favorites_saved_translation(language: Language) -> &'static str {
         Language::TR => "Henüz kaydedilmiş favori yok",
         Language::JA => "お気に入りはまだ保存されていません",
         Language::RO => "Niciun favorit salvat încă",
+        Language::ID => "Belum ada favorit yang tersimpan",
         _ => "No favorites saved yet",
     }
 }
